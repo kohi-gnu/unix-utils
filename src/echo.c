13dbf23e8bc13245bb81fd055291eadbfff61a99
d0p1 <contact@d0p1.eu>
2021-02-25 13:45:41 +0100

    feat: add -n flag to echo

--- src/echo.c
+++ src/echo.c
@@ -1,17 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int
 main(int argc, const char *argv[])
 {
-	int idx;
+	int             idx;
+	int             nflag;
 
-	for (idx = 1; idx < argc; idx++)
+	nflag = 0;
+	idx = 1;
+	if (strcmp(argv[idx], "-n") == 0)
+	{
+		nflag = 1;
+		idx++;
+	}
+
+	while (idx < argc)
 	{
 		if (idx > 1)
 			putchar(' ');
-		fputs(argv[idx], stdout);
+		fputs(argv[idx++], stdout);
 	}
-	putchar('\n');
+	if (!nflag)
+	{
+		putchar('\n');
+	}
 	return (EXIT_SUCCESS);
 }
