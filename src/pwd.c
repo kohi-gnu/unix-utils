999d723b8cee8a56a3b2ebd96aefe898e7d8cef1
d0p1 <contact@d0p1.eu>
2021-12-09 15:02:34 +0100

    feat(pwd): -L & -P flags

--- src/pwd.c
+++ src/pwd.c
@@ -1,14 +1,71 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include <limits.h>
+
+/* XXX: fix me */
+#ifndef PATH_MAX
+# define PATH_MAX 4096
+#endif /*!PATH_MAX*/
 
 int
-main(void)
+logical_pwd(void)
 {
-	char           *buff;
+	char *pwd;
 
-	buff = getcwd(NULL, 0);
-	printf("%s\n", buff);
-	free(buff);
+	pwd = getenv("PWD");
+	if (pwd == NULL)
+	{
+		return (0);
+	}
+
+	if (*pwd != '/' || strlen(pwd) > PATH_MAX)
+	{
+		return (0);
+	}
+
+	if (strstr(pwd, "/..") != NULL)
+	{
+		return (0);
+	}
+	puts(pwd);
+	return (1);
+}
+
+int
+main(int argc, char *argv[])
+{
+	char buff[PATH_MAX];
+	int idx;
+	int logical; /* -L */
+
+	logical = 1; /* POSIX default to -L */
+	if (argc > 1)
+	{
+		for (idx = 1; idx < argc; idx++)
+		{
+			if (strcmp(argv[idx], "-L") == 0)
+			{
+				logical = 1;
+			}
+			else if (strcmp(argv[idx], "-P") == 0)
+			{
+				logical = 0;
+			}
+		}
+	}
+
+	if (logical && logical_pwd())
+	{
+		return (EXIT_SUCCESS);
+	}
+
+	if (getcwd(buff, sizeof(buff)) == NULL)
+	{
+		perror("pwd");
+		return (EXIT_FAILURE);
+	}
+	puts(buff);
 	return (EXIT_SUCCESS);
 }
